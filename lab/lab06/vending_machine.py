4a79d2bdfed21fe568ff141417ff31fc06a4dcc5
Tony Hung <chenyuan_hung@berkeley.edu>
2021-02-01 16:40:27 -0800

    Add files via upload

---
+++ lab/lab06/vending_machine.py
@@ -0,0 +1,26 @@
+def vending_machine(snacks):
+    """Cycles through sequence of snacks.
+
+    >>> vender = vending_machine(('chips', 'chocolate', 'popcorn'))
+    >>> vender()
+    'chips'
+    >>> vender()
+    'chocolate'
+    >>> vender()
+    'popcorn'
+    >>> vender()
+    'chips'
+    >>> other = vending_machine(('brownie',))
+    >>> other()
+    'brownie'
+    >>> vender()
+    'chocolate'
+    """
+    "*** YOUR CODE HERE ***"
+    a=0
+    def fn():
+        nonlocal a
+        c= snacks[a]
+        a=(a+1) % len(snacks)
+        return c
+    return fn
